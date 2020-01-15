724ecfa31da31b03d3eb0e15b55746b1dfe445d4
Courtney Topping <Courtney@Courtneys-MBP.fc.ca>
2020-01-15 10:38:34 -0500

    sprint 2 done posters

--- js/main.js
+++ js/main.js
@@ -19,6 +19,10 @@ var vm = new Vue({
       { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
     ],
 
+videotitle: "video title goes here",
+vidsource: "",
+videodescription: "ivdeo descriptopm here",
+
     showDetails: false
   },
 
@@ -37,6 +41,17 @@ var vm = new Vue({
       // if its true, set the value equal to the left of the colon
       // if its false, set the value equal to the right
       this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    },
+
+    showMovieDetails({name, vidsource, description}) {
+      //console.log('show these details: ', movie);
+
+      this.videotitle = name;
+      this.vidsource = vidsource;
+      this.videodescription = description;
+
+      // make the movie details show up
+      this.showMovieDetails = true;
     }
   }
 });
